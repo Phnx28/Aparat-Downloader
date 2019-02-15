0f0c8e4220b4f40706fea6cc96e21cc54eb37b3e
Phnx28 <39337418+Phnx28@users.noreply.github.com>
2019-02-16 00:39:51 +0330

    Update Aparat Downloader.py
    اضافه شدن دکمه شیشه ای و پیش زمینه خودکار سازی پیدا کردن کیفیت های موجود

--- Aparat Downloader.py
+++ Aparat Downloader.py
@@ -1,6 +1,6 @@
 import telepot
 import telepot.namedtuple
-from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
+from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.loop import MessageLoop
 import time
 import requests
@@ -29,41 +29,68 @@ def handle(msg):
             title = title[0]
 
             dl = []
+            #availableq = []
+            #qu = ['144p', '240p', '480p', '720p', '1080p']
+
             for i in down:
                 reg = re.findall(r'(https.+?)\"', str(i))
                 dl.append(reg[0])
             for i in dl:
                 if '144p' in i:
                     Dl144 = i
+                    #availableq.append(Dl144)
                 elif '240p' in i:
                     Dl240 = i
+                    #availableq.append(Dl240)
                 elif '480p' in i:
                     Dl480 = i
+                    #availableq.append(Dl480)
                 elif '720p' in i:
                     Dl720 = i
+                    #availableq.append(Dl720)
                 elif '1080p' in i:
                     Dl1080 = i
+                    #availableq.append(Dl1080)
 
-            bot.sendMessage(chat_id,('''
-[%s | 144p](%s)
-[%s | 240p](%s)
-[%s | 480p](%s)
-[%s | 720p](%s)''' % (title, Dl144, title, Dl240, title, Dl480, title, Dl720)) , parse_mode='markdown')
+
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                for i in range(availableq):
+                   [InlineKeyboardButton(text='144p', url=Dl144, callback_data='1')],
+                   [InlineKeyboardButton(text='240p', url=Dl240, callback_data='2')],
+                   [InlineKeyboardButton(text='480p', url=Dl480, callback_data='3')],
+                   [InlineKeyboardButton(text='720p', url=Dl720, callback_data='4')],
+                   [InlineKeyboardButton(text='1080p', url=Dl1080, callback_data='5')]
+               ])
+
+            bot.sendMessage(chat_id, ('%s' % title), reply_markup=keyboard)
+
+
+            #bot.sendMessage(chat_id,('''
+#[%s | 144p](%s)
+#[%s | 240p](%s)
+#[%s | 480p](%s)
+#[%s | 720p](%s)''' % (title, Dl144, title, Dl240, title, Dl480, title, Dl720)) , parse_mode='markdown')
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    print('Callback Query:', query_id, from_id, query_data)
+
+    bot.answerCallbackQuery(query_id, text='Got it')
 
 
 def on_inline_query(msg):
     query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
     print ('Inline Query:', query_id, from_id, query_string)
 
+    query_string = query_string.strip('')
     if ' ' in query_string:
-        searchbar = query_string.strip('')
         searchbar = query_string.replace(" ", "_")
         searchurl = ('https://www.aparat.com/search/%s' % (searchbar))
     else:
         searchurl = ('https://www.aparat.com/search/%s' % (query_string))
     ress = requests.get(searchurl)
     soupp = BeautifulSoup(ress.content, 'html.parser')
-    videos = soupp.find_all('div', class_="vide-item__info", limit=10)
+    videos = soupp.find_all('div', class_="vide-item__info", limit=5)
     videoslinks = []
     videostitles = []
     for i in videos:
@@ -123,7 +150,8 @@ def on_chosen_inline_result(msg):
 
 MessageLoop(bot, {'inline_query': on_inline_query,
                   'chosen_inline_result': on_chosen_inline_result,
-                  'chat': handle}).run_as_thread()
+                  'chat': handle,
+                  'callback_query': on_callback_query}).run_as_thread()
 print('I am listening ...')
 
 while 1:
